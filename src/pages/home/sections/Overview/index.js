ae2dfeecf4153ce71a54b49677be3033f05b50e0
rajeshRoul <roul.rajesh28@gmail.com>
2022-04-27 23:45:03 +0530

    Add Aminations and stars in Overview

--- src/pages/home/sections/Overview/index.js
+++ src/pages/home/sections/Overview/index.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import NameContainer from "./components/nameContainer";
 import classes from './style.module.css'
 
 const Overview = () => {
+    const [stars, setStars] = useState([]);
+
+    const randomIntFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    useLayoutEffect(() => {
+        let stars = [];
+        for(let i=0; i<50; ++i){
+            stars.push(
+                <div 
+                    className={classes.star}
+                    style={{
+                        top: `${randomIntFromInterval(0, 100)}%`,
+                        left: `${randomIntFromInterval(0, 100)}%`
+                    }} 
+                />
+            )
+        }
+        for(let i=0; i<50; ++i){
+            stars.push(
+                <div 
+                    className={classes.starSmall}
+                    style={{
+                        top: `${randomIntFromInterval(0, 100)}%`,
+                        left: `${randomIntFromInterval(0, 100)}%`
+                    }} 
+                />
+            )
+        }
+        setStars(stars);
+    }, [])
+
     return (
-        <div className={classes.main}>
+        <div id="overview-container" className={classes.main}>
+            {stars}
             <div className={classes.ctr1}>
                 <NameContainer
                     name={"Rajesh Roul"}
